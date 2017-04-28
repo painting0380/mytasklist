1c44dacaf1b389f0789c9dd02e48794c42d6ea3b
painting <painting0380@gmail.com>
2017-04-28 13:36:11 +0800

    add single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -12,4 +12,14 @@ router.get('/tasks', function(req, res, next){
 	});
 });
 
+
+router.get('/tasks/:id', function(req, res, next){
+	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+		if(err){
+			res.send(err);
+		}
+		res.json(task);
+	});
+});
+
 module.exports = router;
