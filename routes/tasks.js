f222a2ddcd58ac289891efabd1a45ac287918b1b
painting <painting0380@gmail.com>
2017-04-28 13:24:46 +0800

    connect to mLab and show the contents

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://amily:root@ds123351.mlab.com:23351/mytasklist_amily',['tasks']);
 
 router.get('/tasks', function(req, res, next){
-	res.send('TASK API');
+	db.tasks.find(function(err, tasks){
+		if(err){
+			res.send(err);
+		}
+		res.json(tasks);
+	});
 });
 
 module.exports = router;
